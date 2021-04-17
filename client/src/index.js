7a29a811d174d25369936800bcde3bd837295416
amishpr <amish.prajapati@gmail.com>
2021-04-17 18:15:10 -0400

    Setup basic components for Checkout, Signin, and Signup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "gestalt/dist/gestalt.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/App";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Checkout from "./components/Checkout";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route component={App} exact path="/" />
+      <Route component={Signin} path="/signin" />
+      <Route component={Signup} path="/signup" />
+      <Route component={Checkout} path="/checkout" />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
+
