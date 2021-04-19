1f196ef3db6e9e1d0ccf3f84e60bb8f7a2e4e43f
amishpr <amish.prajapati@gmail.com>
2021-04-19 02:17:42 -0400

    Added GraphQL query

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,63 @@
 import React, { Component } from "react";
+import { Container, Box, Heading } from 'gestalt';
 import "./App.css";
+import Strapi from 'strapi-sdk-javascript/build/main';
+const apiUrl = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiUrl);
 
 class App extends Component {
+
+  state = {
+    brands: []
+  }
+
+  async componentDidMount() {
+
+    try {
+      const response = await strapi.request('POST', '/graphql', {
+        data: {
+          query: `query {
+            brands {
+              _id
+              name
+              description
+              createdAt
+              image {
+                url
+                name
+              }
+            }
+          }`
+        }
+      });
+  
+      this.setState({brands: response.data.brands});
+
+    } catch (error) {
+      console.error(error)
+    }
+   
+  }
+
+
   render() {
-    return <div>App</div>;
+    return (
+      <Container>
+        {/* Brands Section */}
+        <Box
+          display="flex"
+          justifyContent="center"
+          marginBottom={2}
+        >
+        
+        {/* Brands Heading */}
+        <Heading color="midnight" size="md">
+          Brew Brands
+        </Heading>
+
+        </Box>
+      </Container>
+    )
   }
 }
 
