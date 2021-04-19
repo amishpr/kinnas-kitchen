4b69cf424f7dad3c281e69146becdf816bbc3f1b
amishpr <amish.prajapati@gmail.com>
2021-04-19 01:08:58 -0400

    Added navigation bar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Box, Text, Heading, Image } from 'gestalt';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+    <Box
+        display="flex"
+        alignItems="center"
+        height={70}
+        justifyContent="around"
+        color="midnight"
+        padding={1}
+        shape="roundedBottom"
+    >
+        {/* Sign in link  */}
+        <NavLink activeClassName="active" exact to="/signin">
+            <Text size="x1" color="white">
+                Signin
+            </Text>
+        </NavLink>
+
+        {/* Title and Logo */}
+        <NavLink activeClassName="active" exact to="/">
+            <Box display="flex" alignItems="center">
+                <Box margin={2} height={50} width={50}>
+                    <Image 
+                        alt="Kinna's Kitchen Logo"
+                        naturalHeight={1}
+                        naturalWidth={1}
+                        src="./icons/logo.svg"
+                    />
+                </Box>
+                <Heading size="xs" color="white">
+                    Kinna's Kitchen
+                </Heading>
+            </Box>
+        </NavLink>
+
+        {/* Sign up link*/}
+        <NavLink activeClassName="active" exact to="/signup">
+            <Text size="x1" color="white">
+                Signup
+            </Text>
+        </NavLink>
+
+    </Box>
+)
+
+export default Navbar;
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "gestalt/dist/gestalt.css";
 
 import App from "./components/App";
+import Navbar from "./components/Navbar";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Checkout from "./components/Checkout";
@@ -12,12 +13,15 @@ import registerServiceWorker from "./registerServiceWorker";
 
 const Root = () => (
   <Router>
+    <React.Fragment>
+    <Navbar />
     <Switch>
       <Route component={App} exact path="/" />
       <Route component={Signin} path="/signin" />
       <Route component={Signup} path="/signup" />
       <Route component={Checkout} path="/checkout" />
     </Switch>
+    </React.Fragment>
   </Router>
 );
 
