45022c3c3da23ec127814523ba9c9ab4e33d3087
amishpr <amish.prajapati@gmail.com>
2021-04-19 06:34:39 -0400

    Removed server configs

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'faad4f17af5a47f45bc1458d2b002710'),
-    },
-  },
-});
