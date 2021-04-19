c0bfc8c386888a93c1266906ce24e411a45faf89
amishpr <amish.prajapati@gmail.com>
2021-04-19 03:14:16 -0400

    Created cards to display items with a search field

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Container, Box, Heading } from 'gestalt';
+import { Container, Box, Heading, Card, Image, Text, SearchField, Icon, Spinner} from 'gestalt';
+import { Link } from 'react-router-dom';
 import "./App.css";
 import Strapi from 'strapi-sdk-javascript/build/main';
 const apiUrl = process.env.API_URL || 'http://localhost:1337';
@@ -8,7 +9,9 @@ const strapi = new Strapi(apiUrl);
 class App extends Component {
 
   state = {
-    brands: []
+    brands: [],
+    searchTerm: "",
+    loadingBrands: true
   }
 
   async componentDidMount() {
@@ -31,18 +34,53 @@ class App extends Component {
         }
       });
   
-      this.setState({brands: response.data.brands});
+      this.setState({brands: response.data.brands, loadingBrands:false});
 
     } catch (error) {
       console.error(error)
+      this.setState({loadingBrands:false});
     }
    
   }
 
+  handleChange = ({ value }) => {
+    this.setState({ searchTerm: value });
+  }
+
+  filterBrands = ( { searchTerm, brands } ) => {
+
+    return brands.filter(brand => {
+      return brand.name.toLowerCase().includes(searchTerm.toLowerCase())
+          || brand.description.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+  }
 
   render() {
+
+    const { searchTerm, loadingBrands } = this.state;
+
     return (
       <Container>
+        {/* Search */}
+        <Box display="flex" justifyContent="center" marginTop={4}>
+          <SearchField
+            id="searchField"
+            accessibilityLabel="Brand Search Field"
+            onChange={this.handleChange}
+            value={searchTerm}
+            placeholder="Search Brands"
+          />
+          <Box margin={3}>
+            <Icon
+              icon="filter"
+              color={searchTerm ? 'orange' : 'gray'}
+              size={20}
+              accessibilityLabel="filter"
+            />
+          </Box>
+        </Box>
+          
         {/* Brands Section */}
         <Box
           display="flex"
@@ -54,8 +92,51 @@ class App extends Component {
         <Heading color="midnight" size="md">
           Brew Brands
         </Heading>
-
         </Box>
+
+        {/* Brands */}
+        <Box wrap display="flex" justifyContent="around">
+          {this.filterBrands(this.state).map(brand => (
+            <Box
+              width={200}
+              margin={2}
+              key={brand._id}
+            >
+              <Card
+                image = {
+                  <Box height={200} width={200}>
+                    <Image
+                      fit="cover"
+                      alt={`${brand.name}`}
+                      naturalHeight={1}
+                      naturalWidth={1}
+                      src={`${apiUrl}${brand.image[0].url}`}
+                    />
+                  </Box>
+                }
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Text bold size="xl">{brand.name}</Text>
+                  <Text>{brand.description}</Text>
+                  <Text bold size="xl">
+                    <Link to={`/${brand._id}`}> See Brews </Link>
+                  </Text>
+                </Box>
+           
+              </Card>
+
+            </Box>  
+          ))}
+        </Box>
+        <Spinner
+          show={loadingBrands}
+          accessibilityLabel="Loading Spinner"
+        />
       </Container>
     )
   }
